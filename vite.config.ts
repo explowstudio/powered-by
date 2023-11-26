c9d2a2bbc3d26ac7515bd2ea1b4214744ff96600
oigabrielteodoro <gabriel.teodoro@tino.com.br>
2023-11-25 21:56:03 -0300

    feat: add workflow to new version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import * as packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve("src", "react", "index.ts"),
+      name: "PoweredBy",
+      formats: ["es", "umd"],
+      fileName: (format) => `explow-studio-powered-by.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.dependencies)],
+    },
+  },
+});
